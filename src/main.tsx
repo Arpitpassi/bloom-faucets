c4fd4d8bc583f6abb36b5a3714b48f48149185ef
ArpitPassi <43519412+Arpitpassi@users.noreply.github.com>
2025-07-02 18:39:02 +0530

    works

--- src/main.tsx
+++ src/main.tsx
@@ -1,27 +1,28 @@
-import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App.tsx"
 import "./globals.css"
 import { ArweaveWalletKit } from "arweave-wallet-kit"
 import { UserProvider } from "./hooks/useUser.tsx"
+// import AoSyncStrategy from "@vela-ventures/aosync-strategy" // Import AoSyncStrategy
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ArweaveWalletKit
-      config={{
-        permissions: ["ACCESS_ADDRESS", "SIGN_TRANSACTION", "DISPATCH", "SIGNATURE"],
-        ensurePermissions: true,
-        appInfo: {
-          name: "Bloom Pool Manager",
-        },
-      }}
-    >
-      <UserProvider>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </UserProvider>
-    </ArweaveWalletKit>
-  </React.StrictMode>
+  <ArweaveWalletKit
+    config={{
+      permissions: [
+        "ACCESS_ADDRESS",
+        "ACCESS_PUBLIC_KEY", // Added for Beacon Wallet support
+        "SIGN_TRANSACTION",
+        "DISPATCH",
+      ],
+      ensurePermissions: true,
+      // strategies property removed as it is not supported by Config type
+    }}
+  >
+    <UserProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </UserProvider>
+  </ArweaveWalletKit>
 )
